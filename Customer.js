114575128edc093ff683cf4d052177b9fb76d92e
redricasa <fredeb26@gmail.com>
2019-10-24 22:25:02 -0700

    added the displaytable function to display the updated table after the user selects how much of the chosen product they want to buy

--- Customer.js
+++ Customer.js
@@ -44,8 +44,9 @@ function amount (answers) {
         {
             type: "input",
             name: "amount",
-            message: "How many of the " + answers.wearables + " do you want to buy?",
-            // checks if user entry is a number or not, and more than 0
+            message: `How many of the ${answers.wearables} do you want to buy?`,
+            //TO DO: do a connction query 
+            // checks if user entry is not a number, and whether it is more than 0
             validate: function(value){
                 if(isNaN(value)){
                     console.log('\n You need to provide a number')
@@ -63,31 +64,54 @@ function amount (answers) {
 function query (answers, item){
     // console.log("number to buy : ",item);
     // console.log("product name : ",answers)
+    var wearables = [answers.wearables];
     var select = "SELECT * FROM bamazon_db.products WHERE product_name = ?"; 
-    connect.query(select, answers.wearables, function(error, response){
+    connect.query(select, wearables, function(error, response){
         if (error) throw error;
         var price = response[0].price;
         //gets the stock_quantity of the product and parses it from a string into a number
         var userAmount = parseInt(item.amount);
         var total = price * userAmount;
-        console.log("Your TOTAL is : ", total);
-        var inventory = response[0].stock_quantity;
+        var inventory = [response[0].stock_quantity];
         //get the difference b/n stock-quantity and user number
         var difference = inventory - userAmount;
-        console.log("user chose to buy these #'s of product: ",userAmount)
+        // console.log("user chose to buy these #'s of the product: ",userAmount)
         if ( difference < 0 ){
-            console.log("Insufficient quantity!")
+            console.log(`Insufficient quantity! \n Choose a number lower than ${inventory}`)
             amount(answers);
         }else{
             //run query to update table
-            var update = "UPDATE products SET stock_quantity = ? WHERE product_name = ?"
-            connect.query( update , difference, answers.wearables, function(error, res){
+            // var update = "UPDATE bamazon_db.products SET stock_quantity = ? WHERE product_name = ?"
+            var params = [difference, answers.wearables];
+            connect.query( "UPDATE bamazon_db.products SET stock_quantity = ? WHERE product_name = ?", params, function(error, res){
                 if (error) throw error;
+                console.log(`Your TOTAL is : $${total}`);
+                keepShopping();                
             });
         }
         return; 
     })
 };
+function keepShopping(){
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'store',
+            message: 'Would you like to shop or exit?',
+            choices: ['SHOP', 'EXIT'],
+        }
+    ]).then(function(choice){
+        if(choice.store === ('EXIT')){
+            connect.end(function(err){
+                if (err) throw err;
+                console.log('connection to mysql ENDED!');
+            });
+        }else{
+            console.log("Here's the updated inventory to choose from: ")
+            displayTable();
+        }
+    })
+}
 // prompts if user wants to enter the store or exit
 function promptMenu() {
     console.log('Welcome to the Bamazon store!')
